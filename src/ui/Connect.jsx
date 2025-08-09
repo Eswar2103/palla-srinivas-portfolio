4a5ade040bfbae5d2547cc826b367d8fb2fd9362
Kummithi Guru Eswar Sainath Reddy <kummithi.sai00@gmail.com>
2025-08-09 22:55:23 +0530

    Connect with leader page

--- src/ui/Connect.jsx
+++ src/ui/Connect.jsx
@@ -1,87 +1,132 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { useRef, useState } from "react";
+import { Form } from "react-router-dom";
 
 function Connect() {
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [isLenghtExcluded, setIsLengthExcluded] = useState(false);
+  const formRef = useRef(null);
   function handleChange(e) {
     const maxWords = 250;
-    if (!e?.target?.value) {
-      return;
-    }
     const inputText = e.target.value;
     const words = inputText.trim().split(/\s+/);
-    console.log('words---', words);
     if (words.length <= maxWords) {
-      console.log('hiiii');
       setDescription(inputText);
       setIsLengthExcluded(false);
     } else {
       setIsLengthExcluded(true);
     }
   }
+
+  function handleFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    formRef.current.reset();
+    setDescription("");
+    console.log("data-----:", data);
+  }
   return (
-    <div className="mx-auto my-12 max-w-3xl p-8">
-      <Form method="POST">
-        <div className="mb-5 flex flex-col justify-center sm:flex-row sm:items-center">
-          <label htmlFor="name" className="sm:basis-30">
-            Full Name
-          </label>
-          <input
-            className="input"
-            name="name"
-            type="text"
-            required
-            placeholder="user name"
-          />
-        </div>
-        <div className="mb-5 flex flex-col justify-center sm:flex-row sm:items-center">
-          <label htmlFor="phone" className="sm:basis-30">
-            Phone Number
-          </label>
-          <input
-            className="input"
-            name="phone"
-            type="tel"
-            required
-            placeholder="phone number"
-          />
-        </div>
-        <div className="mb-5 flex flex-col justify-center sm:flex-row sm:items-center">
-          <p className="sm:basis-30">Choose option</p>
-          <div className="flex flex-row items-center gap-x-6">
-            <div className="flex gap-x-1">
-              <input type="radio" name="suggestion" id="suggestion" />
-              <label htmlFor="suggestion">Suggestion</label>
-            </div>
-            <div className="flex gap-x-1">
-              <input type="radio" name="complaint" id="complaint" />
-              <label htmlFor="complaint">Complaint</label>
-            </div>
-          </div>
-        </div>
-        <div className="mb-5 flex flex-col justify-center sm:flex-row">
-          <label className="sm:basis-30" htmlFor="description">
-            Description
-          </label>
-          <div className="flex w-1/2 flex-col">
-            <textarea
-              className="description"
-              name="description"
+    <div className="flex justify-evenly items-center bg-stone-300/20">
+      <div className="flex my-4 py-4">
+        <Form
+          method="POST"
+          onSubmit={handleFormData}
+          ref={formRef}
+          onKeyDown={(e) => e.key === "Enter" && handleFormData}
+        >
+          <div className="input-base">
+            <label htmlFor="name" className="sm:basis-30">
+              Full Name
+            </label>
+            <input
+              className="input"
+              name="name"
               type="text"
-              placeholder="Enter upto 250 words..."
-              onChange={handleChange}
-              value={description}
               required
+              placeholder="full name"
             />
-            {isLenghtExcluded && (
-              <p className="mt-2 mb-2 rounded-full bg-red-100 px-2 py-1 text-center text-xs text-red-500 sm:text-sm">
-                Only 250 words are allowed to send as a message
-              </p>
-            )}
           </div>
-        </div>
-      </Form>
+          <div className="input-base">
+            <label htmlFor="phone" className="sm:basis-30">
+              Phone Number
+            </label>
+            <input
+              className="input"
+              name="phone"
+              type="tel"
+              pattern="[0-9]{10}"
+              maxLength="10"
+              required
+              placeholder="phone number"
+              onInvalid={(e) =>
+                e.target.setCustomValidity(
+                  "Please enter a valid 10-digit phone number"
+                )
+              }
+              onChange={(e) => e.target.setCustomValidity("")}
+            />
+          </div>
+          <div className="input-base">
+            <div className="flex flex-row items-center gap-x-6">
+              <div className="flex gap-x-1">
+                <input
+                  type="radio"
+                  name="type"
+                  id="suggestion"
+                  value="suggestion"
+                />
+                <label htmlFor="suggestion">Suggestion</label>
+              </div>
+              <div className="flex gap-x-1">
+                <input type="radio" name="type" id="concern" value="concern" />
+                <label htmlFor="concern">Concern</label>
+              </div>
+              <div className="flex gap-x-1">
+                <input
+                  type="radio"
+                  name="type"
+                  id="complaint"
+                  value="feedback"
+                />
+                <label htmlFor="feedback">Feedback</label>
+              </div>
+            </div>
+          </div>
+          <div className="input-base">
+            <label className="sm:basis-30" htmlFor="description">
+              Description
+            </label>
+            <div className="flex w-1/2 flex-col">
+              <textarea
+                className="description"
+                name="description"
+                type="text"
+                placeholder="Enter upto 250 words..."
+                onChange={handleChange}
+                value={description}
+                required
+              />
+              {isLenghtExcluded && (
+                <p className="mt-2 mb-2 rounded-full bg-red-100 px-2 py-1 text-center text-xs text-red-500 sm:text-sm">
+                  Only 250 words are allowed to send as a message
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="input-base">
+            <span className="sm:basis-40"></span>
+            <button
+              className="rounded-xl w-xl font-bold border border-black/40 px-3 py-1 bg-amber-300 hover:bg-amber-500 left-2"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      </div>
+      <div className="max-w-lg flex">
+        <p className="break-words">hdhdhdh</p>
+      </div>
     </div>
   );
 }
