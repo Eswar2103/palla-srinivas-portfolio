c7629890d13e49baed9696ddcdf9654e1715508b
Guru Eswar sainath Reddy Kummithi <kummithi.sai00@gmail.com>
2025-09-23 22:49:44 +0530

    SPP page data

--- src/ui/PartyPresident.jsx
+++ src/ui/PartyPresident.jsx
@@ -1,5 +1,29 @@
 function PartyPresident() {
-  return <div>Party president</div>;
+  return (
+    <section className="px-6 sm:px-40 py-10">
+      <div className="flex flex-col items-center gap-y-3 py-4 border-amber-500 border-4 rounded-2xl">
+        <div className="flex flex-col sm:flex-row gap-x-20 gap-y-6 px-2 justify-center">
+          <img
+            className="w-[400px] h-[400px]"
+            src="./SPP2.jpg"
+            alt="palla srinivas and chandra babu naidu photo"
+          />
+          <img
+            className="w-[400px] h-[400px]"
+            src="./SPP_photo.jpg"
+            alt="palla srinivas and lokesh photo"
+          />
+        </div>
+        <p className="text-xl px-2">
+          Taken charge as <span className="font-bold">Telugu Desam Party</span>{" "}
+          president on{" "}
+          <span className="text-red-700 uppercase font-bold">
+            JUNE 28, 2014
+          </span>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default PartyPresident;
